daaaa2fa8afb5c90789e295cdb61939a1b7ee743
quotesun <quotesun@gmail.com>
2021-05-24 21:44:59 +0300

    create new config file for get config using dot notation

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -2,6 +2,46 @@
 
 namespace Mjolnir\Config;
 
-use Mjolnir\Abstracts\AbstractRepository;
+use Mjolnir\App;
+use Mjolnir\Support\Arr;
 
-class Config extends AbstractRepository {}
+class Config
+{
+
+    /**
+     * @var App
+     */
+    private $container;
+    /**
+     * @var string
+     */
+    private $identifier;
+
+    public function __construct(App $container, string $identifier)
+    {
+        $this->container = $container;
+        $this->identifier = $identifier;
+    }
+
+    public static function get(App $container, string $identifier)
+    {
+        return (new self($container, $identifier))->resolveIdentifier();
+    }
+
+    private function resolveIdentifier()
+    {
+        $keys = explode('.', $this->identifier);
+        $firstKey = Arr::first($keys);
+        $config = $this->container->get('config');
+
+        if (!$config->has($firstKey)) {
+            return null;
+        }
+
+        $identifierWithoutFirst = implode('.', Arr::except($keys, 0));
+        $items = $config->get($firstKey)
+            ->toArray();
+
+        return Arr::get($items, $identifierWithoutFirst);
+    }
+}
