e043780df76f19cc870d99e778dea07682b46bce
quotesun <quotesun@gmail.com>
2021-05-26 09:16:11 +0300

    add hook service provider

---
+++ src/Providers/HooksServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mjolnir\Providers;
+
+use Exception;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+use Mjolnir\Abstracts\AbstractServiceProvider;
+use Mjolnir\App;
+use Mjolnir\Hooks\Action;
+use Mjolnir\Hooks\Filter;
+use Mjolnir\Hooks\HookRepository;
+
+class HooksServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    /** @var App */
+    protected $container;
+
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function boot()
+    {
+        $this->container->share('hooks', HookRepository::class);
+        $this->container->share('action', Action::class)
+            ->addArguments(['app' => $this->container, 'action' => 'add_action']);
+        $this->container->share('filter', Filter::class)
+            ->addArguments(['app' => $this->container, 'action' => 'add_filter']);
+    }
+}
